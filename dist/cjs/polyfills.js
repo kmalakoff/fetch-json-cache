0c9bca0ebf48f91305c93295a611ea7b4f77c2b6
Kevin Malakoff <kmalakoff@gmail.com>
2024-04-24 12:17:58 -0700

    Upgrade modules

---
+++ dist/cjs/polyfills.js
@@ -0,0 +1,3 @@
+"use strict";
+require("core-js/actual/promise");
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }

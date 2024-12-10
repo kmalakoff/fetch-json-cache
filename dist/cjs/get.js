fd7a71de981fdb15572dc332dca37882c7f8d341
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-09 21:43:06 -0800

    Upgrade modules

--- dist/cjs/get.js
+++ dist/cjs/get.js
@@ -28,4 +28,4 @@ module.exports = function getCache(endpoint, callback) {
         }
     });
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }

e74fc8682d4358387935fa22d9fb30764a8756ad
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 01:02:32 -0800

    Add TypeScript types and migrate to mkdirp-classic

--- src/get.ts
+++ src/get.ts
@@ -4,6 +4,14 @@ import get from 'get-remote';
 
 import update from './update.js';
 
+interface GetError {
+  code?: string;
+}
+
+interface GetHeaders {
+  etag?: string;
+}
+
 export default function getCache(endpoint, callback) {
   const fullPath = path.join(this.cacheDirectory, `${this.options.hash(endpoint)}.json`);
   fs.readFile(fullPath, (err, contents) => {
@@ -19,10 +27,10 @@ export default function getCache(endpoint, callback) {
       get(endpoint).head((err, res) => {
         // not accessible
         if (err) {
-          if (err.code === 'ENOTFOUND' || err.message.indexOf('routines:SSL23_GET_SERVER_HELLO') >= 0) return callback(null, record.body);
+          if ((err as GetError).code === 'ENOTFOUND' || err.message.indexOf('routines:SSL23_GET_SERVER_HELLO') >= 0) return callback(null, record.body);
           return callback(err);
         }
-        if (res.headers.etag === record.headers.etag) return callback(null, record.body);
+        if ((res.headers as GetHeaders).etag === record.headers.etag) return callback(null, record.body);
         update.call(this, endpoint, (err, json) => {
           err ? callback(err) : callback(null, json);
         });

b6fb5fccc29ffea977a423c2be3a5d5fbff6e1ee
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-25 03:27:16 -0800

    Replace write-file-atomic with a queued temp file rename in update

--- src/update.ts
+++ src/update.ts
@@ -1,7 +1,10 @@
+import fs from 'fs';
 import path from 'path';
 import get from 'get-remote';
 import mkdirp from 'mkdirp-classic';
-import writeFile from 'write-file-atomic';
+import Queue from 'queue-cb';
+import rimraf2 from 'rimraf2';
+import tempSuffix from 'temp-suffix';
 
 export default function update(endpoint, callback) {
   const fullPath = path.join(this.cacheDirectory, `${this.options.hash(endpoint)}.json`);
@@ -11,10 +14,17 @@ export default function update(endpoint, callback) {
 
     get(endpoint).json((err, res) => {
       if (err) return callback(err);
-
       const record = { headers: res.headers, body: res.body };
-      writeFile(fullPath, JSON.stringify(record), 'utf8', (err) => {
-        err ? callback(err) : callback(null, record.body);
+      const tempFile = `${fullPath}-${tempSuffix()}`;
+
+      const queue = new Queue(1);
+      queue.defer(fs.writeFile.bind(null, tempFile, JSON.stringify(record), 'utf8'));
+      queue.defer((cb) => rimraf2(fullPath, { disableGlob: true }, cb.bind(null, null)));
+      queue.defer(fs.rename.bind(null, tempFile, fullPath));
+      queue.await((err) => {
+        if (!err) return callback(null, record.body);
+        // cleanup the temp file if the operation failed
+        rimraf2(tempFile, { disableGlob: true }, callback.bind(err));
       });
     });
   });

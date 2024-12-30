06a34e7fc0df4eaeaf6ee8b5a69f89b61cb78c6f
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 23:51:56 -0800

    Rename cacheDirectory to cachePath and refactor worker logic

--- src/getSync.ts
+++ src/getSync.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function getCacheAsync(endpoint) {
-  const fullPath = path.join(this.cacheDirectory, `${this.options.hash(endpoint)}.json`);
+  const fullPath = path.join(this.cachePath, `${this.options.hash(endpoint)}.json`);
   try {
     const contents = fs.readFileSync(fullPath, 'utf8');
     const record = JSON.parse(contents.toString());
--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,13 @@ export type GetCallback = (error?: Error, json?: object) => void;
 export type ClearCallback = (error?: Error) => void;
 
 export default class Cache {
-  cacheDirectory: string;
+  cachePath: string;
   options: CacheOptions;
 
-  constructor(cacheDirectory: string, options: CacheOptions = {}) {
+  constructor(cachePath: string, options: CacheOptions = {}) {
     if (!(this instanceof Cache)) throw new Error('Cache needs to be called with new');
-    if (!cacheDirectory) throw new Error('Cache needs cacheDirectory');
-    this.cacheDirectory = cacheDirectory;
+    if (!cachePath) throw new Error('Cache needs cachePath');
+    this.cachePath = cachePath;
     this.options = options;
     if (!this.options.hash) this.options.hash = hash;
   }
@@ -34,12 +34,16 @@ export default class Cache {
     }
     options = options || {};
 
-    if (typeof callback === 'function') return (options as GetOptions).force ? update.call(this, endpoint, callback) : get.call(this, endpoint, callback);
-    return new Promise((resolve, reject) => {
-      this.get(endpoint, options, (err, json) => {
+    function worker(endpoint, options, callback) {
+      (options as GetOptions).force ? update.call(this, endpoint, callback) : get.call(this, endpoint, callback);
+    }
+
+    if (typeof callback === 'function') return worker.call(this, endpoint, options, callback) as undefined;
+    return new Promise((resolve, reject) =>
+      worker.call(this, endpoint, options, (err, json) => {
         err ? reject(err) : resolve(json);
-      });
-    });
+      })
+    );
   }
 
   getSync(endpoint: string): object | null {
@@ -48,15 +52,16 @@ export default class Cache {
   }
 
   clear(callback?: ClearCallback): undefined | Promise<undefined> {
-    if (typeof callback === 'function')
-      return rimraf2(this.cacheDirectory, { disableGlob: true }, (_err) => {
-        _err = null; // ignore errors since it is fine to delete a directory that doesn't exist from a cache standpoint
-        callback();
-      });
-    return new Promise((resolve, reject) => {
-      this.clear((err) => {
+    function worker(callback) {
+      // ignore errors since it is fine to delete a directory that doesn't exist from a cache standpoint
+      rimraf2(this.cachePath, { disableGlob: true }, callback.bind(null, null));
+    }
+
+    if (typeof callback === 'function') return worker.call(this, callback);
+    return new Promise((resolve, reject) =>
+      worker.call(this, (err) => {
         err ? reject(err) : resolve(undefined);
-      });
-    });
+      })
+    );
   }
 }

a0a6a09c944e96f41c599d6ccc4833d5514e583b
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:22:37 -0700

    Switch to mocha for testing and update promise test logic

--- test/spec/promise.test.ts
+++ test/spec/promise.test.ts
@@ -17,15 +17,13 @@ interface DistTagsJSON {
 describe('promise', () => {
   (() => {
     // patch and restore promise
-    // @ts-ignore
-    let rootPromise: Promise;
+    if (typeof global === 'undefined') return;
+    const globalPromise = global.Promise;
     before(() => {
-      rootPromise = global.Promise;
-      // @ts-ignore
       global.Promise = Pinkie;
     });
     after(() => {
-      global.Promise = rootPromise;
+      global.Promise = globalPromise;
     });
   })();
 

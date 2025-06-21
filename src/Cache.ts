4a9ee9e5c86af61700dd2ac31bb85cae4842b8a5
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 09:39:46 -0700

    Comprehensive types

---
+++ src/Cache.ts
@@ -0,0 +1,54 @@
+import rimraf2 from 'rimraf2';
+
+import get from './get.ts';
+import getSync from './getSync.ts';
+import hash from './hash.ts';
+import type { CacheOptions, ClearCallback, GetCallback, GetOptions } from './types.ts';
+import update from './update.ts';
+
+export default class Cache {
+  cachePath: string;
+  options: CacheOptions;
+
+  constructor(cachePath: string, options: CacheOptions = {}) {
+    if (!(this instanceof Cache)) throw new Error('Cache needs to be called with new');
+    if (!cachePath) throw new Error('Cache needs cachePath');
+    this.cachePath = cachePath;
+    this.options = options;
+    if (!this.options.hash) this.options.hash = hash;
+  }
+
+  get(endpoint: string, options?: GetOptions | GetCallback, callback?: GetCallback): undefined | Promise<object | null> {
+    if (typeof options === 'function') {
+      callback = options as GetCallback;
+      options = null;
+    }
+    options = options || {};
+
+    function worker(endpoint, options, callback) {
+      (options as GetOptions).force ? update.call(this, endpoint, callback) : get.call(this, endpoint, callback);
+    }
+
+    if (typeof callback === 'function') return worker.call(this, endpoint, options, callback) as undefined;
+    return new Promise((resolve, reject) => worker.call(this, endpoint, options, (err, json) => (err ? reject(err) : resolve(json))));
+  }
+
+  getSync(endpoint: string): object | null {
+    // TODO: add async fetching
+    return getSync.call(this, endpoint);
+  }
+
+  clear(callback?: ClearCallback): undefined | Promise<undefined> {
+    function worker(callback) {
+      // ignore errors since it is fine to delete a directory that doesn't exist from a cache standpoint
+      rimraf2(this.cachePath, { disableGlob: true }, callback.bind(null, null));
+    }
+
+    if (typeof callback === 'function') return worker.call(this, callback);
+    return new Promise((resolve, reject) =>
+      worker.call(this, (err) => {
+        err ? reject(err) : resolve(undefined);
+      })
+    );
+  }
+}

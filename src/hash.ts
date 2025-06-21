ba5a746af7e8a2bd4625e093e4946cfd6d60376f
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 09:39:46 -0700

    Comprehensive types

--- src/hash.ts
+++ src/hash.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
 
-export default function hash(data) {
+export default function hash(data: string): string {
   return crypto.createHash('md5').update(data).digest('hex');
 }

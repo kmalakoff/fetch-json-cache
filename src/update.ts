66aef06db35b0b0fb2426897f9ca10f09503f59b
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-15 16:47:04 -0800

    Migrate to get-file-compat to simpllify implementation

--- src/update.ts
+++ src/update.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { rm } from 'fs-remove-compat';
-import get from 'get-remote';
+import { getContent } from 'get-file-compat';
 import mkdirp from 'mkdirp-classic';
 import path from 'path';
 import Queue from 'queue-cb';
@@ -14,20 +14,26 @@ export default function update<T>(endpoint: string, callback: UpdateCallback<T>)
   mkdirp(this.cachePath, (err) => {
     if (err && err.code !== 'EEXIST') return callback(err);
 
-    get(endpoint).json((err, res) => {
+    getContent(endpoint, 'utf8', (err, res) => {
       if (err) return callback(err);
-      const record = { headers: res.headers, body: res.body } as Record<T>;
-      const tempFile = `${fullPath}-${tempSuffix()}`;
 
-      const queue = new Queue(1);
-      queue.defer(fs.writeFile.bind(null, tempFile, JSON.stringify(record), 'utf8'));
-      queue.defer((cb) => rm(fullPath, cb.bind(null, null)));
-      queue.defer(fs.rename.bind(null, tempFile, fullPath));
-      queue.await((err) => {
-        if (!err) return callback(null, record.body);
-        // cleanup the temp file if the operation failed
-        rm(tempFile, callback.bind(err));
-      });
+      try {
+        const body = JSON.parse(res.content);
+        const record = { headers: res.headers, body } as Record<T>;
+        const tempFile = `${fullPath}-${tempSuffix()}`;
+
+        const queue = new Queue(1);
+        queue.defer(fs.writeFile.bind(null, tempFile, JSON.stringify(record), 'utf8'));
+        queue.defer((cb) => rm(fullPath, cb.bind(null, null)));
+        queue.defer(fs.rename.bind(null, tempFile, fullPath));
+        queue.await((err) => {
+          if (!err) return callback(null, record.body);
+          // cleanup the temp file if the operation failed
+          rm(tempFile, callback.bind(err));
+        });
+      } catch (err) {
+        return callback(err);
+      }
     });
   });
 }
